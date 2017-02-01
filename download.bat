faf306d7b85a7e861819c6b5a5a90be4574ffe75
James Griesen <griesen18j@ncssm.edu>
2017-01-31 23:37:52 -0500

    james Tue 01/31/2017 23:37:52.37

--- download.bat
+++ download.bat
@@ -1,9 +1,4 @@
-@echo off
-set /p user=Who is this?  (lowercase first letter):
-if %user%==j (set user=james;echo Hello James)
-if %user%==c(set user=cameron;echo Hello Cameron)
-if %user%==r(set user=raymond;echo Hello Raymond)
-if %user%==d(set user=david;echo Fuck you David)
+call getUser
 git checkout master
 git pull origin master
 git checkout %user%
--- save.bat
+++ save.bat
@@ -1,20 +1,4 @@
-set /p user=Who is this?  (lowercase first letter):
-if ("%user%"=="j")
-(
-     set user=james
-)
-if ("%user%"=="c")
-(
-     set user=cameron
-)
-if ("%user%"=="r")
-(
-     set user=raymond
-)
-if ("%user%"=="d")
-(
-     set user=david
-)
+call getUser
 git checkout %user%
 git add -A
 git commit -m "%user% %date% %time%"
